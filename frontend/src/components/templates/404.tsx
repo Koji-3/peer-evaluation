0ecce541a3069dcd087e55dd3e31824cf9936804
ayakayakak <bayakau.tka@gmail.com>
2023-09-17 16:55:20 +0900

    404をデザイン通りに

--- frontend/src/components/templates/404.tsx
+++ frontend/src/components/templates/404.tsx
@@ -28,7 +28,34 @@ const StyledWrapper = styled.div`
     }
 
     .to-top {
-      margin: 0 auto;
+      margin: 0 auto 14.6rem;
+    }
+
+    .about-404 {
+      font-size: 1.2rem;
+
+      .title {
+        margin: 0 0 2.5rem;
+        font-weight: 700;
+        text-align: center;
+      }
+
+      .content {
+        margin: 0 0 3rem;
+        line-height: 2rem;
+        letter-spacing: 0.06rem;
+      }
+
+      .ref {
+        font-size: 1rem;
+        line-height: 2rem;
+        letter-spacing: 0.05rem;
+        opacity: 0.5;
+
+        > a {
+          text-decoration: underline;
+        }
+      }
     }
   }
 `
@@ -44,7 +71,33 @@ export const Page404Tpl: React.FC<Props> = ({ className }) => {
       <div className="inner">
         <p className="code">404</p>
         <p className="description">アクセスしたページは削除されたか、URLが変更されているため表示することができません。</p>
-        <Button buttonText="トップへ" buttonType="primary" onClick={goToTop} className="to-top" />
+        <Button buttonText="ホームへ" buttonType="primary" onClick={goToTop} className="to-top" />
+
+        <div className="about-404">
+          <p className="title">▼404とは</p>
+          <p className="content">
+            404とは、インターネット上からページが削除されるなどして表示できないことを示す。
+            <br />
+            <br />
+            WWW（world wide
+            web）を開発したひとり、ロバート・カイリューいわく、「具体的な数字はプログラマーの気分で決められていった。」とのこと。
+            <br />
+            クライアントエラーを示すステータスコードが400番台に決まり、これといった理由はなく404が「Not Found」に指定された。
+            <br />
+            <br />
+            404の数字の意味には「実は404号室に由来している」「その部屋は世界初のウェブサーヴァーが置かれたところで、スイスの欧州原子核研究機構（CERN）内にある」などの様々な逸話がある。
+            <br />
+            そのようなエピソードに対して、
+            <br />
+            「そんなものは完全なる“神話”なのさ。新システム用のコーディングをしているとき、（ページが削除されたといったような）エラー検出に備えたメッセージを長々と書いている余裕なんてないんだ。64キロバイトのメモリーでプログラミングするのがどういうことか、いまのギークたちにはわからないだろうね」とカイリューははねつけた。
+          </p>
+          <p className="ref">
+            引用元：
+            <a href="https://wired.jp/2018/01/25/history-of-the-404-error/" target="_blank" rel="noopener noreferrer">
+              「404」は部屋番号だった？ 「Not Found」エラーにまつわる噂の真偽を、「生みの親」に聞いてみた
+            </a>
+          </p>
+        </div>
       </div>
     </StyledWrapper>
   )
